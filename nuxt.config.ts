baaef755b90aa564defe78435c821c19e7927a2f
Dimasadhyaksa <143145756+Dimasadhyaksa@users.noreply.github.com>
2026-02-06 06:50:09 +0700

    Add files via upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,18 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
+  modules: ['@nuxtjs/google-fonts'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+
+   googleFonts: {
+    families: {
+      Inter: [400, 500, 600, 700],
+    },
+    display: 'swap'
   },
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Cal Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
